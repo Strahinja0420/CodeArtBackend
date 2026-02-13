6c5d7965ed1af95249fe6043a8674f56187c68a3
Strahinja420 <strahidelijica1@gmail.com>
2026-02-13 16:21:50 +0100

    feat: implemented rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { UserModule } from './user/user.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, SupabaseModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    SupabaseModule,
+    UserModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
